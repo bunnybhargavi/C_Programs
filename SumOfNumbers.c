396d7bca4aac02a1842a50f58ec3003308ee5970
Bhargavi Amillineni <147232054+bunnybhargavi@users.noreply.github.com>
2024-03-06 20:47:44 +0530

    Add files via upload

---
+++ SumOfNumbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum=0;
+    printf("Enter the values of the variables a and b :");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("Sum of two numbers is %d",sum);
+}
