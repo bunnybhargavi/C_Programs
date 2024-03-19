b635c7db580d73bf22a0842dbfcf11474cf75e81
Bhargavi Amillineni <147232054+bunnybhargavi@users.noreply.github.com>
2024-03-19 23:27:32 +0530

    Add files via upload

---
+++ Pattern2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int rows;
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+    for (int i=1; i<=rows; i++){
+        for (int j=1; j<= 2* (rows-i) ; j++){
+            printf(" ");
+        }
+        for (int k=1; k<2*i; k++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+}
